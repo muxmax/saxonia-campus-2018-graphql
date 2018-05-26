5adcc024777679511ea4bcdf73e54aeb9baf6e9f
maxe.brat@web.de <maxe.brat@web.de>
2018-05-26 15:08:58 +0200

    Introduce router to implement application routing.

--- bloggie-react/src/App.js
+++ bloggie-react/src/App.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ArticlesStream from "./components/ArticlesStream";
+import {Route, BrowserRouter as Router} from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bloggie React Frontend mit Apollo</h1>
-        </header>
-        <ArticlesStream/>
-      </div>
+        <Router>
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo"/>
+              <h1 className="App-title">Bloggie React Frontend mit Apollo</h1>
+            </header>
+            <Route path="/" exact component={ArticlesStream}/>
+          </div>
+        </Router>
     );
   }
 }
